2796771e2db57770c0570544eef96adab7491c6c
lequanghao <haolq2002@gmail.com>
2022-12-29 21:43:28 +0700

    Thêm controller và view Blog, BlogSingle

---
+++ VegeFoods/Controllers/BlogSingleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VegeFoods.Controllers
+{
+    public class BlogSingleController : Controller
+    {
+        // GET: BlogSingle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
