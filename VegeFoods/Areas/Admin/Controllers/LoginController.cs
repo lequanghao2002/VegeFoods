1ecf7121dd9a09eba5581474b912754ea3fce7f3
lequanghao <haolq2002@gmail.com>
2023-01-10 15:35:52 +0700

    Xử lí login

--- VegeFoods/Areas/Admin/Controllers/LoginController.cs
+++ VegeFoods/Areas/Admin/Controllers/LoginController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VegeFoods.Models.AccountModel;
+using VegeFoods.Models.BD_VegeFoods;
 
 namespace VegeFoods.Areas.Admin.Controllers
 {
@@ -12,6 +14,31 @@ namespace VegeFoods.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login(User model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = new AccountModel().Login(model.Account, model.Password);
+                
+                if (result)
+                {
+                    var user = new AccountModel().getID(model.Account);
+                    var userSession = new Session();
+                    userSession.UserID = user.ID;
+                    userSession.UserName = user.FullName;
+
+                    Session.Add("User", userSession);
+
+                    return RedirectToAction("List","Role");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect account or password");
+                }
+            }
+            return View("Index");
         }
     }
 }
