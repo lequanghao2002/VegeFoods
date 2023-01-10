382ad55b30634e678c89b297382771b3c0422bdd
lequanghao <haolq2002@gmail.com>
2023-01-10 15:37:00 +0700

    Kiểm tra: Nếu chưa login admin thì không vào trang admin

---
+++ VegeFoods/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using VegeFoods.Models.AccountModel;
+
+namespace VegeFoods.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Admin/Base
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = (Session)Session["User"];
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(
+                        new { controller = "Login", action = "Index", Area = "Admin" }
+                    ));
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
