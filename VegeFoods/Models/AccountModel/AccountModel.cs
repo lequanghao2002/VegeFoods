7750199edd4e4cf53f4f224a84d03c9da5b9520c
lequanghao <haolq2002@gmail.com>
2023-01-10 15:34:50 +0700

    Hàm xử lí các login

---
+++ VegeFoods/Models/AccountModel/AccountModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VegeFoods.Models.BD_VegeFoods;
+
+namespace VegeFoods.Models.AccountModel
+{
+    public class AccountModel
+    {
+        DB_VegeFoodEntities db = null;
+        public AccountModel() {
+            db = new DB_VegeFoodEntities();
+        }
+
+        public User getID(string accountName)
+        {
+            return db.Users.SingleOrDefault(m => m.Account == accountName);
+        }
+
+        public int Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Login(string account, string password)
+        {
+            var result = db.Users.Count(m => m.Account== account 
+                                        && m.Password == password
+                                        /*&& m.Role_ID == 1*/);
+            if(result > 0 )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
