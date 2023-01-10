5a70fb9b65f0ea2c586fd92dadee77a4c68bf34f
lequanghao <haolq2002@gmail.com>
2023-01-10 15:34:08 +0700

    Tạo session cho admin

---
+++ VegeFoods/Models/AccountModel/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VegeFoods.Models.BD_VegeFoods;
+
+namespace VegeFoods.Models.AccountModel
+{
+    [Serializable]
+    public class Session
+    {
+        public int UserID { set; get; }
+        public string UserName { set; get; }
+    }
+}
