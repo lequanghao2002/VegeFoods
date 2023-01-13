1c93961bad2bb464eda9a38d25a7bd7aedba287d
lequanghao <haolq2002@gmail.com>
2023-01-13 11:33:53 +0700

    Thêm sửa xóa Category

---
+++ VegeFoods/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VegeFoods.Models.AdminModel;
+using VegeFoods.Models.BD_VegeFoods;
+
+namespace VegeFoods.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryModel categoryModel = new CategoryModel();
+        public ActionResult Index()
+        {
+            return View(categoryModel.getListAllCategory());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(categoryModel.checkCategoryName(model.Name) > 0)
+                {
+                    ModelState.AddModelError("", "Name already exists");
+                }
+                else
+                {
+                    var result = categoryModel.Insert(model);
+                    if (result > 0)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Category create failed");
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(categoryModel.findCategoryById(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (categoryModel.checkCategoryName(model.Name) > 1)
+                {
+                    ModelState.AddModelError("", "Name already exists");
+                }
+                else
+                {
+                    if (categoryModel.Update(model))
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Category update failed");
+                    }
+                }
+            }
+            return View(model);
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            categoryModel.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ VegeFoods/Models/AdminModel/CategoryModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VegeFoods.Models.BD_VegeFoods;
+
+namespace VegeFoods.Models.AdminModel
+{
+    public class CategoryModel
+    {
+        DB_VegeFoodEntities db = null;
+        public CategoryModel()
+        {
+            db = new DB_VegeFoodEntities();
+        }
+        public List<Category> getListAllCategory()
+        {
+            return db.Categories.ToList();
+        }
+
+        public int checkCategoryName(string name)
+        {
+            return db.Categories.Count(m => m.Name == name);
+        }
+
+        public Category findCategoryById(int id)
+        {
+            return db.Categories.Find(id);
+        }
+
+        public int Insert(Category entity)
+        {
+            db.Categories.Add(entity);
+            db.SaveChanges();
+
+            return entity.ID;
+        }
+
+        public bool Update(Category entity)
+        {
+            try
+            {
+                var updateModel = findCategoryById(entity.ID);
+                updateModel.Name = entity.Name;
+
+                db.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var deleteModel = findCategoryById(id);
+                db.Categories.Remove(deleteModel);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
