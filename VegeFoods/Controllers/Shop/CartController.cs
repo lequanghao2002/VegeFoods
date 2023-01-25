52d700294b0b10690da0dc618dd1abb4aa696e46
lequanghao <haolq2002@gmail.com>
2023-01-25 10:17:54 +0700

    Tạo và thêm giỏ hàng

--- VegeFoods/Controllers/Shop/CartController.cs
+++ VegeFoods/Controllers/Shop/CartController.cs
@@ -3,15 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VegeFoods.Models.AdminModel;
+using VegeFoods.Models.BD_VegeFoods;
+using VegeFoods.Models.CustomerModel;
 
 namespace VegeFoods.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
+        private const string CartSession = "CartSession";
         public ActionResult Index()
         {
-            return View();
+            var cartSession = Session[CartSession];
+            var cartList = new List<CartModel>();
+            if (cartSession != null)
+            {
+                cartList = (List<CartModel>)cartSession;
+            }
+            return View(cartList);
+        }
+        public ActionResult AddItem(int productId, int quantity)
+        {
+            var cartSession = Session[CartSession];
+            var product = new ProductModel().getProductById(productId);
+
+            if (cartSession != null)
+            {
+                var cartList = (List<CartModel>)cartSession;
+                if (cartList.Exists(x => x.product.ID == productId))
+                {
+                    foreach (var item in cartList)
+                    {
+                        if (item.product.ID == productId)
+                        {
+                            item.quantity += quantity;
+                        }
+                    }
+                }
+                else
+                {
+                    var item = new CartModel();
+                    item.product = product;
+                    item.quantity = quantity;
+
+                    cartList.Add(item);
+
+                }
+
+                Session[CartSession] = cartList;
+            }
+            else
+            {
+                var item = new CartModel();
+                item.product = product;
+                item.quantity = quantity;
+
+                var cartList = new List<CartModel>();
+                //*****************
+                cartList.Add(item);
+
+                Session[CartSession] = cartList;
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ VegeFoods/Models/CustomerModel/CartModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using VegeFoods.Models.BD_VegeFoods;
+
+namespace VegeFoods.Models.CustomerModel
+{
+    [Serializable]
+    public class CartModel
+    {
+        public Product product { get; set; }
+        public int quantity { get; set; }
+    }
+
+    
+}
