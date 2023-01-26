c9d9d720ba6a662041794833c2d189a57d2de476
lequanghao <haolq2002@gmail.com>
2023-01-26 22:14:17 +0700

    Hiển thị tiền sản phẩm, giảm giá, tổng tiền ở CheckOut

--- VegeFoods/Controllers/Shop/CheckOutController.cs
+++ VegeFoods/Controllers/Shop/CheckOutController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VegeFoods.Models.CustomerModel;
 
 namespace VegeFoods.Controllers
 {
     public class CheckOutController : Controller
     {
         // GET: CheckOut
+        private const string CartSession = "CartSession";
         public ActionResult Index()
         {
-            return View();
+            var cartSession = Session[CartSession];
+            var cartList = new List<CartModel>();
+            if (cartSession != null)
+            {
+                cartList = (List<CartModel>)cartSession;
+            }
+            return View(cartList);
         }
     }
 }
