201be862027332827bf1a1d4b7b3b16a80bbeee4
Quang Hào <haolq2002@gmail.com>
2023-07-23 12:39:27 +0700

    update

--- VegeFoods/Models/BD_VegeFoods/VegeFoodsModel.Designer.cs
+++ VegeFoods/Models/BD_VegeFoods/VegeFoodsModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\itYu\LTWeb\VegeFoods\VegeFoods\VegeFoods\Models\BD_VegeFoods\VegeFoodsModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ITMain\VegeFoods\VegeFoods\VegeFoods\Models\BD_VegeFoods\VegeFoodsModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
